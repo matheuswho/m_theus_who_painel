52d6e26be8a76d6a3d2f1c6e4365cd75da133bff
m_theus_who <98497633+matheuswho@users.noreply.github.com>
2026-02-24 14:31:50 -0300

    Update readfile function to use new URL

--- delta/init.lua
+++ delta/init.lua
@@ -1,8 +1,5 @@
--- delta_cheat/delta/init.lua
-
--- Delta Executor aceita loadstring direto; só garante que readfile funcione
 if not readfile then
     function readfile(p)
-        return game:HttpGet("https://raw.githubusercontent.com/YOUR_USER/YOUR_REPO/main/"..p)
+        return game:HttpGet("https://raw.githubusercontent.com/matheuswho/m_theus_who_painel/main/"..p)
     end
 end
